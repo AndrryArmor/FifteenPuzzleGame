e435c259d488afb962ebca0ea47b134e756d5371
AndrryArmor <andrry1@gmail.com>
2020-11-21 09:00:40 +0200

    Writes code to BusinessLayer.Entities

--- BusinessLayer.Entities/GameEngine.cs
+++ BusinessLayer.Entities/GameEngine.cs
@@ -17,22 +17,50 @@ namespace FifteenPuzzleGame.BusinessLayer.Entities
 
         public static GameEngine CreateInstance()
         {
-
+            if (_gameEngine == null)
+                _gameEngine = new GameEngine();
+            return _gameEngine;
         }
 
         bool MakeMove(GameField gameField, Direction direction)
         {
+            Tile neighbourTile = GetNeighbourTileByDirection(gameField, gameField.SpaceTile, direction);
+            if (neighbourTile == null)
+                return false;
 
+            Tile spaceTile = gameField.SpaceTile;
+            Swap(ref spaceTile, ref neighbourTile);
+            return true;
         }
 
-        private Tile GetNeighbourhoodTileByDirection(GameField gameField, Tile tile, Direction direction)
+        private Tile GetNeighbourTileByDirection(GameField gameField, Tile tile, Direction direction)
         {
-
+            Tile neighbourTile = null;
+            switch (direction)
+            {
+                case Direction.Up:
+                    if (tile.Row > 0)
+                        neighbourTile = new Tile(tile.Row - 1, tile.Column, tile.Value);
+                    break;
+                case Direction.Right:
+                    if (tile.Column < gameField.Columns - 1)
+                        neighbourTile = new Tile(tile.Row, tile.Column + 1, tile.Value);
+                    break;
+                case Direction.Down:
+                    if (tile.Row < gameField.Rows - 1)
+                        neighbourTile = new Tile(tile.Row + 1, tile.Column, tile.Value);
+                    break;
+                case Direction.Left:
+                    if (tile.Column > 0)
+                        neighbourTile = new Tile(tile.Row, tile.Column - 1, tile.Value);
+                    break;
+            }
+            return neighbourTile;
         }
 
         private void Swap(ref Tile left, ref Tile right)
         {
-
+            (left, right) = (right, left);
         }
     }
 }
--- BusinessLayer.Entities/GameField.cs
+++ BusinessLayer.Entities/GameField.cs
@@ -8,12 +8,31 @@ namespace FifteenPuzzleGame.BusinessLayer.Entities
 {
     public class GameField
     {
-        public Tile[] Field { get; }
+        public Tile[,] Field { get; }
         public Tile SpaceTile { get; }
+        public int Rows => Field.GetLength(0);
+        public int Columns => Field.GetLength(1);
 
         public GameField(int rows, int columns)
         {
+            Field = new Tile[rows, columns];
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    int value = i * columns + j + 1;
+                    Field[i, j] = new Tile(i, j, value);
+                }
+            }
 
+            SpaceTile = new Tile(rows - 1, columns - 1, 0);
+            Field[rows - 1, columns - 1] = SpaceTile;
+        }
+
+        public Tile this[int row, int column]
+        {
+            get => Field[row, column];
+            set => Field[row, column] = value;
         }
     }
 }
