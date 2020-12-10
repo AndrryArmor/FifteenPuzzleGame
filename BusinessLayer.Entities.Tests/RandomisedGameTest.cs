386bc5f981c1862b08c2d665fc9b0ea48f4d6bf9
AndrryArmor <andrry1@gmail.com>
2020-12-10 07:48:07 +0200

    Adds RandomisedGameTest class

---
+++ BusinessLayer.Entities.Tests/RandomisedGameTest.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using FifteenPuzzleGame.BusinessLayer.Abstract;
+using FifteenPuzzleGame.BusinessLayer.Entities;
+using FifteenPuzzleGame.BusinessLayer.Impl.Games;
+using FifteenPuzzleGame.BusinessLayer.Impl.Services;
+using Moq;
+using NUnit.Framework;
+
+namespace FifteenPuzzleGame.BusinessLayer.Tests
+{
+    public class RandomisedGameTest
+    {
+        private RandomisedGame _mockRandomisedGame;
+        private GameEngine _mockGameEngine;
+        private GameField _mockGameField;
+
+        [SetUp]
+        public void Setup()
+        {
+            _mockGameEngine = GameEngine.CreateInstance();
+            _mockGameField = new GameField(3, 3);
+        }
+
+        [Test]
+        public void MakeMove_MoveTileUp_ShouldIncreaseAmountMovesWhenMoveIsPossible()
+        {
+            _mockRandomisedGame = new RandomisedGame(_mockGameEngine, _mockGameField);
+            int expected = _mockRandomisedGame.Moves + 1;
+
+            _mockRandomisedGame.MakeMove(Direction.Up);
+
+            Assert.AreEqual(expected, _mockRandomisedGame.Moves);
+        }
+
+        [Test]
+        public void MakeMove_MoveTileUp_ShouldNotIncreaseAmountMovesWhenMoveIsImpossible()
+        {
+            _mockRandomisedGame = new RandomisedGame(_mockGameEngine, _mockGameField);
+            int expected = _mockRandomisedGame.Moves;
+
+            _mockRandomisedGame.MakeMove(Direction.Up);
+
+            Assert.AreEqual(expected, _mockRandomisedGame.Moves);
+        }
+    }
+}
